17c1a530bc646780881daadfcbe87b23d81809c1
nilsonmendes <nilson.fmmendes@outlook.com>
2024-06-13 23:21:09 +0200

    Create NavbarLink component

--- first-app/src/components/Navbar.vue
+++ first-app/src/components/Navbar.vue
@@ -4,8 +4,10 @@
             <a class="navbar-brand" href="#"> My Vue </a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li v-for="(page, index) in pages" class="nav-item" :key="index">
-                    <a class="nav-link" :class="{active: activePage == index}" aria-current="page" :href="page.link.url" 
-                    :title="'This link goes to the ${page.link.text} page'" @click.prevent="navLinkClick(index)"> {{ page.link.text }}</a>
+                    <navbar-link :isActive = "activePage == index"
+                        :page=pages[index] 
+                        @click.prevent="navLinkClick(index)"> 
+                    </navbar-link>
                 </li>
             </ul> 
             <form class="d-flex"> 
@@ -18,7 +20,11 @@
 </template>
 
 <script> 
+import NavbarLink from './NavbarLink.vue';
 export default { 
+    components: {
+        NavbarLink
+    },
     props: ['pages', 'activePage', 'navLinkClick'],
     data() {
         return{ 
---
+++ first-app/src/components/NavbarLink.vue
@@ -0,0 +1,18 @@
+<template>
+    <a class="nav-link" :class="activeClasses" aria-current="page" :title="`This link goes to the ${page.link.text} page`" :href="page.link.url"> {{ page.link.text }}
+    </a>
+</template>
+
+
+<script>
+    export default {
+        props: ['isActive', 'page'],
+        computed: {
+            activeClasses(){
+                return{  
+                    active: this.isActive
+                }
+            } 
+        }
+    }
+</script>
