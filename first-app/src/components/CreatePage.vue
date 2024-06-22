a85e4b27b5764785bcff62b246275a91e87a26c3
nilsonmendes <nilson.fmmendes@outlook.com>
2024-06-22 18:13:35 +0200

    Include new fields and validation on form

--- first-app/src/components/CreatePage.vue
+++ first-app/src/components/CreatePage.vue
@@ -1,36 +1,83 @@
 <template>
-    <div class="container mb-3"> 
-        <form action="">
-            <div class="mb-3">
-                <label for="" class="form-label">
-                    Page Title
-                </label>
-                <input v-model="pageTitle" type="text" class="form-control"/>
+    <form action="" class="container mb-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Page Title
+                    </label>
+                    <input v-model="pageTitle" type="text" class="form-control"/>
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Content
+                    </label>
+                    <textarea v-model="content" type="text" class="form-control" rows="5"> 
+                    </textarea>
+                </div>
+            </div>
+            <div class="col">
+                <div class=mb-3>
+                    <label for="" class="form-label">
+                        Link text
+                    </label>
+                    <input type="text" v-model="linkText" class="form-control"/> 
+                </div>
+                <div class=mb-3>
+                    <label for="" class="form-label">
+                        Link URL
+                    </label>
+                    <input type="text" v-model="linkUrl" class="form-control"/> 
+                </div>
+                <div class="row col-mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" for="checkboxs"/>
+                        <label class="form-check-label" for="gridCheck1"> 
+                            Published
+                        </label>  
+                    </div>
+                </div>
             </div>
             <div class="mb-3">
-                <label for="" class="form-label">
-                    Content
-                </label>
-                <textarea v-model="content" type="text" class="form-control" rows="5"> 
-                </textarea>
-            </div>
-            <div class="mb-3">
-                <button class="btn btn-primary" @click.prevent="pageCreated({pageTitle, content})">
+                <button :disabled="isFormInvalid" class="btn btn-primary" @click.prevent="submitForm()">
                     Create Page
                 </button>
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
 </template>
 
 <script> 
     export default {
         props:['pageCreated'],
-
+        computed: {
+            isFormInvalid(){ 
+                return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+            }
+        },
         data() { 
             return {
                 pageTitle: '',
-                content: ''
+                content: '',
+                linkText: '',
+                linkUrl: '',
+            }
+        },
+        methods: {
+            submitForm(){ 
+                if(!this.pageTitle || !this.content || !this.linkText || !this.linkUrl){
+                    alert('Please fill the form');
+                    return;
+                }
+
+                this.pageCreated({
+                    pageTitle: this.pageTitle,
+                    content: this.content,
+                    link: { 
+                        text: this.linkText,
+                        url: this.linkUrl,
+                    }
+                });
             }
         }
     }
