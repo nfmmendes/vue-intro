fa0840f4029d5c8c6ddd9969eb531e845e9a104a
nilsonmendes <nilson.fmmendes@outlook.com>
2024-07-07 18:20:01 +0200

    Configure events component on main page

--- first-app/src/main.js
+++ first-app/src/main.js
@@ -1,6 +1,11 @@
 import {createApp, VueElement} from 'vue'
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import $bus from './utils/Events';
 
-let app = createApp(App).mount("#app");
+let app = createApp(App);
+
+app.config.globalProperties.$bus = $bus;
+
+app.mount("#app");
 
