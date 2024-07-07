20f238de2a03aaa71c555466bf1be773a0d0ea59
nilsonmendes <nilson.fmmendes@outlook.com>
2024-07-07 15:51:52 +0200

    Make pageCreated an event

--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
    <navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index"></navbar>
    <page-viewer v-if="pages.length > 0" :page="pages[activePage]">  </page-viewer>
-   <create-page :page-created="pageCreated"></create-page>
+   <create-page @page-created="pageCreated"></create-page>
 </template>
 
 <script> 
