e1ed4c6411ea79a0322c58218cc24ba14b3b43a0
nilsonmendes <nilson.fmmendes@outlook.com>
2024-07-12 15:20:37 +0200

    Set pages as global property

--- first-app/src/main.js
+++ first-app/src/main.js
@@ -3,12 +3,14 @@ import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import $bus from './utils/Events';
 import router from './utils/routes.js';
+import $pages from './utils/data.js';
 
 let app = createApp(App);
 
 app.use(router);
 
 app.config.globalProperties.$bus = $bus;
+app.config.globalProperties.$pages = $pages;
 
 app.mount("#app");
 
