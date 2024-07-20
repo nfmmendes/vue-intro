b78f27a6a81d9c3c2a023da2c639cc2072e2c8b2
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-20 20:12:35 +0200

    Move route.js out of utils folder

--- first-app/src/main.js
+++ first-app/src/main.js
@@ -2,7 +2,7 @@ import {createApp, VueElement} from 'vue'
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import $bus from './utils/Events';
-import router from './utils/routes.js';
+import router from './routes.js';
 import $pages from './utils/data.js';
 
 let app = createApp(App);
