7acd3a2ce0eea833ae59387f6c96ebf5c6429166
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-20 23:53:45 +0200

    Change app to use vue-route

--- first-app/src/main.js
+++ first-app/src/main.js
@@ -2,8 +2,11 @@ import {createApp, VueElement} from 'vue'
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import $bus from './utils/Events';
+import router from './routes.js';
 
 let app = createApp(App);
+
+app.use(router);
 
 app.config.globalProperties.$bus = $bus;
 
