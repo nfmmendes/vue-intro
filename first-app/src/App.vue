7bb79c6829be19175f6d1a545c20e288c7ef66e4
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-20 23:53:42 +0200

    Add CreatePage component

--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -1,17 +1,19 @@
 <template>
    <navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index"></navbar>
    <page-viewer v-if="pages.length > 0" :page="pages[activePage]">  </page-viewer>
+   <create-page :page-created="pageCreated"></create-page>
 </template>
 
 <script> 
 import Navbar from './components/Navbar.vue'
-
 import PageViewer from './components/PageViewer.vue'
+import CreatePage from './components/CreatePage'
 
 export default { 
     components: {
         Navbar, 
-        PageViewer
+        PageViewer,
+        CreatePage        
     },
     created(){ 
         this.getPages()
@@ -28,6 +30,9 @@ export default {
             let data = await res.json();
 
             this.pages = data;
+        },
+        pageCreated(newPage){ 
+            console.log(newPage)
         }
     }
 }
---
+++ first-app/src/components/CreatePage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container mb-3"> 
+        <form action="">
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Page Title
+                </label>
+                <input v-model="pageTitle" type="text" class="form-control"/>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Content
+                </label>
+                <textarea v-model="content" type="text" class="form-control" rows="5"> 
+                </textarea>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary" @click.prevent="pageCreated({pageTitle, content})">
+                    Create Page
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script> 
+    export default {
+        props:['pageCreated'],
+
+        data() { 
+            return {
+                pageTitle: '',
+                content: ''
+            }
+        }
+    }
+</script>
