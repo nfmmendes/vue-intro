bfdf15618a08d66d9d5dfeb0a01e62f591db289b
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-20 19:36:30 +0200

    Remoe components from App

--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -5,14 +5,10 @@
 
 <script> 
 import Navbar from './components/Navbar.vue'
-import PageViewer from './components/PageViewer.vue'
-import CreatePage from './components/CreatePage'
 
 export default { 
     components: {
-        Navbar, 
-        PageViewer,
-        CreatePage        
+        Navbar      
     },
     methods: { 
         pageCreated(newPage){ 
