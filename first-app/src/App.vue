c04bb90d487832cd87c2059364bf25c2499d0fda
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-20 19:32:08 +0200

    Move pages management from App to Navbar

--- first-app/src/App.vue
+++ first-app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-   <navbar :pages="pages" :active-page="activePage"></navbar>
+   <navbar></navbar>
    <router-view></router-view>
 </template>
 
@@ -14,30 +14,9 @@ export default {
         PageViewer,
         CreatePage        
     },
-    created(){ 
-        this.getPages();
-
-        this.$bus.$on('navbarLinkActivated', (index) => { 
-            this.activePage = index;
-            console.log(this.activePage);
-        });
-    },
-    data(){ 
-        return {
-            activePage: 0,
-            pages: []
-        };
-    },
     methods: { 
-        async getPages() {
-            let res = await fetch('pages.json');
-            let data = await res.json();
-
-            this.pages = data;
-        },
         pageCreated(newPage){ 
             this.pages.push(newPage);
-            console.log(this.pages);
         }
     }
 }
--- first-app/src/components/Navbar.vue
+++ first-app/src/components/Navbar.vue
@@ -5,8 +5,7 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <navbar-link v-for="(page, index) in publishedPages" class="nav-item" :key="index"
                         :index="publishedIndexes[index]" 
-                        :page="page"
-                        @activated="$emit('activated')"> 
+                        :page="page"> 
                     </navbar-link>
 
                     <li>
@@ -30,9 +29,11 @@ export default {
     components: {
         NavbarLink
     },
-    props: ['pages', 'activePage'],
+    props: ['pages'],
     created(){
         this.getThemeSettings(); 
+
+        this.pages = this.$pages.getAllPages();
     },
     computed:{ 
         publishedPages(){ 
@@ -44,7 +45,8 @@ export default {
     },
     data() {
         return{ 
-            theme: 'light'
+            theme: 'light',
+            pages: []
         };
     },
     methods: { 
