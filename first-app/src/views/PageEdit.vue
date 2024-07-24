3a58b258fc87ac3169e8900fe5b09210ffe4b5a8
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-24 18:20:07 +0200

    Include form on page edit

--- first-app/src/views/PageEdit.vue
+++ first-app/src/views/PageEdit.vue
@@ -1,5 +1,49 @@
 <template>
     Edit page {{ router.params.index }}
+
+    <form action="" class="container mb-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Page Title
+                    </label>
+                    <input v-model="pageTitle" type="text" class="form-control"/>
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Content
+                    </label>
+                    <textarea v-model="content" type="text" class="form-control" rows="5"> 
+                    </textarea>
+                </div>
+            </div>
+            <div class="col">
+                <div class=mb-3>
+                    <label for="" class="form-label">
+                        Link text
+                    </label>
+                    <input type="text" v-model="linkText" class="form-control"/> 
+                </div>
+                <div class="row col-mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" v-model="published"/>
+                        <label class="form-check-label" for="gridCheck1"> 
+                            Published
+                        </label>  
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <button :disabled="isFormInvalid" class="btn btn-primary">
+                    Edit
+                </button>
+                <button :disabled="isFormInvalid" class="btn btn-primary">
+                    Cancel
+                </button>
+            </div>
+        </div>
+    </form>
 </template>
 
 <script setup>
